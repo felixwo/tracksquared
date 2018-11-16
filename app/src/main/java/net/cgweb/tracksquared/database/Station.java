0b74d677a71563c8d349f92c8a577f5f09893023
Felix <felixwortmann99@gmail.com>
2018-11-16 19:01:43 +0100

    datamodels

--- app/src/main/java/net/cgweb/tracksquared/database/Station.java
+++ app/src/main/java/net/cgweb/tracksquared/database/Station.java
@@ -3,4 +3,6 @@ package net.cgweb.tracksquared.database;
 public class Station {
     private String stationName;
     private String stationCode;
+
+
 }
--- app/src/main/java/net/cgweb/tracksquared/database/User.java
+++ app/src/main/java/net/cgweb/tracksquared/database/User.java
@@ -4,4 +4,28 @@ public class User {
     private String name;
     private Journey currentJourney;
     private long userSince; //init on first launch
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Journey getCurrentJourney() {
+        return currentJourney;
+    }
+
+    public void setCurrentJourney(Journey currentJourney) {
+        this.currentJourney = currentJourney;
+    }
+
+    public long getUserSince() {
+        return userSince;
+    }
+
+    public void setUserSince(long userSince) {
+        this.userSince = userSince;
+    }
 }
